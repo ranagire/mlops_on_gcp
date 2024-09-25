f18e3b7f5a603bdb7553134d101e8e0c10a30462
Rajesh_Nagireddy <124651336+ranagire@users.noreply.github.com>
2024-09-25 13:15:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 client = bigquery.Client()
 
 @app.route('/')
+
 def main(big_query_client=client):
     table_id = "dao-aa-poc-uyim.test_schema.us_states"
     job_config = bigquery.LoadJobConfig(
